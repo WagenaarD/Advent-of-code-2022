7ec0b5bd95d94c9c77c88e57b6c4ac7a5fa0a5e8
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-04 12:19:55 +0100

    day_04 completed

---
+++ day_04/main.py
@@ -0,0 +1,39 @@
+"""
+Advent of code challenge 2022
+"""
+
+__project__   = 'Advent of code 2022'
+__author__    = 'D W'
+
+import re
+
+
+def line_contains(line: str) -> bool:
+    """
+    Input: a string with four numbers representing two ranges (e.g. 1-4, 5-7).
+    Tests if one of these ranges FULLY contains the other range. This is the case if for one range
+    the low-end is <= the other low_end and the high-end is >= the other high end.
+    """
+    match_list = [int(match) for match in re.findall('[0-9]+', line)]
+    return (match_list[0] <= match_list[2] and match_list[1] >= match_list[3]) or \
+        (match_list[0] >= match_list[2] and match_list[1] <= match_list[3])
+
+    
+def line_has_overlap(line: str) -> bool:
+    """
+    Input: a string with four numbers representing two ranges (e.g. 1-4, 5-7).
+    Tests if one of these ranges has ANY overlap with the other range. This is the case if any 
+    number is present in the other range.
+    """
+    match_list = [int(match) for match in re.findall('[0-9]+', line)]
+    return (match_list[3] >= match_list[0] and match_list[2] <= match_list[0]) or \
+        (match_list[2] <= match_list[1] and match_list[3] >= match_list[1]) or \
+        (match_list[1] >= match_list[2] and match_list[0] <= match_list[2]) or \
+        (match_list[0] <= match_list[3] and match_list[1] >= match_list[3])
+
+
+if __name__ == '__main__':
+    """Executed if file is executed but not if file is imported."""
+    input_lines = open('input.txt').read().split('\n')
+    print('Part 1 result: {}'.format([line_contains(line) for line in input_lines].count(True)))
+    print('Part 2 result: {}'.format([line_has_overlap(line) for line in input_lines].count(True)))
