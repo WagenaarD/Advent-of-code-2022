594f92a31749cb84e196feabc248bed7db2ea657
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-05 12:56:37 +0100

    day_05 part 1

---
+++ day_05/main.py
@@ -0,0 +1,35 @@
+"""
+Advent of code challenge 2022
+Start 12:27
+Part 1: 12:56
+"""
+
+__project__   = 'Advent of code 2022'
+__author__    = 'D W'
+
+(warehouse_input, moves_input) = open('input.txt').read().split('\n\n')
+
+# Get the warehouse input
+warehouse = [[] for _ in range((warehouse_input.find('\n') + 1) // 4)]
+for line in warehouse_input.split('\n')[:-1]:
+    for idx in range(1, len(line), 4):
+        if line[idx] != ' ':
+            warehouse[idx // 4].append(line[idx])
+print(warehouse)
+
+# Process moves
+for move in moves_input.split('\n'):
+    (num_to_move, from_stack, to_stack) = [int(num) for num in move.split() if num.isdigit()]
+    
+    print(num_to_move, from_stack, to_stack)
+    warehouse[to_stack - 1] = list(reversed(warehouse[from_stack - 1][:num_to_move])) + warehouse[to_stack - 1]
+    print(warehouse)
+    del warehouse[from_stack - 1][:num_to_move]
+    print(warehouse)
+
+print('Part 1 result: {}'.format(''.join([stack[0] for stack in warehouse])))
+
+
+
+
+
