4607e2ddf7f2bce4b5098447a84de296e4693b72
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-05 13:07:58 +0100

    Code cleanup

--- day_05/main.py
+++ day_05/main.py
@@ -10,31 +10,24 @@ __author__    = 'D W'
 
 import copy
 
-(warehouse_input, moves_input) = open('input.txt').read().split('\n\n')
+(state_input, moves_input) = open('input.txt').read().split('\n\n')
 
-# Process the warehouse input. warehouse_1 is for part_1, warehouse_2 for part_2
-warehouse_1 = [[] for _ in range((warehouse_input.find('\n') + 1) // 4)]
-for line in warehouse_input.split('\n')[:-1]:
+# Process the state input. state_1 is for part_1, state_2 for part_2
+state_1 = [[] for _ in range((state_input.find('\n') + 1) // 4)]
+for line in state_input.split('\n')[:-1]:
     for idx in range(1, len(line), 4):
         if line[idx] != ' ':
-            warehouse_1[idx // 4].append(line[idx])
-warehouse_2 = copy.deepcopy(warehouse_1)
+            state_1[idx // 4].append(line[idx])
+state_2 = copy.deepcopy(state_1)
 
 # Process moves
 for move in moves_input.split('\n'):
     (num_to_move, from_stack, to_stack) = [int(num) for num in move.split() if num.isdigit()]
-    warehouse_1[to_stack - 1] = list(reversed(warehouse_1[from_stack - 1][:num_to_move])) \
-        + warehouse_1[to_stack - 1]
-    warehouse_2[to_stack - 1] = warehouse_2[from_stack - 1][:num_to_move] \
-        + warehouse_2[to_stack - 1]
-    del warehouse_1[from_stack - 1][:num_to_move]
-    del warehouse_2[from_stack - 1][:num_to_move]
+    state_1[to_stack - 1] = state_1[from_stack - 1][:num_to_move][::-1] + state_1[to_stack - 1]
+    state_2[to_stack - 1] = state_2[from_stack - 1][:num_to_move] + state_2[to_stack - 1]
+    del state_1[from_stack - 1][:num_to_move]
+    del state_2[from_stack - 1][:num_to_move]
 
 # Return result
-print('Part 1 result: {}'.format(''.join([stack[0] for stack in warehouse_1])))
-print('Part 2 result: {}'.format(''.join([stack[0] for stack in warehouse_2])))
-
-
-
-
-
+print('Part 1 result: {}'.format(''.join([stack[0] for stack in state_1])))
+print('Part 2 result: {}'.format(''.join([stack[0] for stack in state_2])))
