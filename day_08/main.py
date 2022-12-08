c40ca77cc3fcd45546fab74d127bbb2cf392aa16
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-08 23:49:13 +0100

    day_08 even cleaner. cleaner than HF?

--- day_08/main.py
+++ day_08/main.py
@@ -10,12 +10,13 @@ import itertools as it
 input_lines = open('input.txt').read().split('\n')
 tree_height = [[int(char) for char in line] for line in input_lines]
 
+no_rows, no_cols = len(tree_height), len(tree_height[0])
 no_visible_trees, max_scenic_score = 0, 0
-for row, col in it.product(range(len(tree_height)), range(len(tree_height[0]))):
+for row, col in it.product(range(no_rows), range(no_cols)):
     score, visible = 1, False
     for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-        tree_line = [tree_height[row + dx * d][col + dy * d] for d in range(1, len(tree_height)) \
-            if 0 <= row + dx * d < len(tree_height) and 0 <= col + dy * d < len(tree_height[0])]
+        valid = lambda d: 0 <= row + dx * d < no_rows and 0 <= col + dy * d < no_cols
+        tree_line = [tree_height[row + dx*d][col + dy*d] for d in it.takewhile(valid, it.count(1))]
         if not any([tree >= tree_height[row][col] for tree in tree_line]):
             visible = True
         for idx, tree in enumerate(tree_line):
