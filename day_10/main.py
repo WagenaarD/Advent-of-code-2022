7737d44eb71d8e8814d67af73f46ae99c063def1
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-10 09:24:56 +0100

    day_10 part 2

--- day_10/main.py
+++ day_10/main.py
@@ -1,9 +1,9 @@
 """
 Advent of code challenge 2022
 >> python3 main.py < in
-Start   - 
+Start   - 09:56
 Part 1  - 09:12
-Part 2  - 
+Part 2  - 09:23
 Cleanup - 
 """
 
@@ -18,10 +18,8 @@ import pprint
 input = sys.stdin.read().strip().split('\n')
 
 x, cycle = 1, 1
-# x_values = []
 x_values = {cycle: x}
 for line in input:
-    print(cycle, x, line)
     if line == 'noop':
         cycle += 1
     else:
@@ -30,10 +28,18 @@ for line in input:
         cycle += 2
         x += amount
     x_values[cycle] = x
-print(cycle, x, 'done')
-
-print([x_values[c] for c in (20, 60, 100, 140, 180, 220)])
-print([c * x_values[c] for c in (20, 60, 100, 140, 180, 220)])
 print('Part 1:', sum([c * x_values[c] for c in (20, 60, 100, 140, 180, 220)]))
+
+output = ''
+for cycle, x in x_values.items():
+    if (cycle - 1) % 40 + 1 in [x, x+1, x+2]:
+        output += '#'
+    else:
+        output += '.'
+    if cycle >= 240:
+        break
+    elif cycle % 40 == 0:
+        output += '\n'
+print('Part 2:\n' + output)
 
 
