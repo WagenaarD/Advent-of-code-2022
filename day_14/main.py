b0af0fd85db6c551d9b28606c1caf97b5335365a
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-14 18:13:41 +0100

    day_14 cleanup

--- day_14/main.py
+++ day_14/main.py
@@ -2,9 +2,14 @@
 Advent of code challenge 2022
 >> python3 main.py < in
 Start   - 
-Part 1  - 
-Part 2  - 
+Part 1  - 795
+Part 2  - 30214
 Cleanup - 
+
+Not a very fast implementation. Keeping a set and storing all positions is faster for part 1 but 
+slower for part 2 compared to keeping track of a grid and all items in it (how Tamara did it). Code
+had to be optimized as it would initially take too long (minutes to reach 10.000 and slowing down 
+each iteration). Current version was 0.07s for part 1, and 50s for part 2.
 """
 
 import sys
@@ -23,12 +28,11 @@ def visualize_grid(rocks: set, sand: set = set()) -> None:
     x_max = max([pair[0] for pair in rocks | sand])
     y_min = min([pair[1] for pair in rocks | sand])
     y_max = max([pair[1] for pair in rocks | sand])
-    print('(x_min, x_max), (y_min, y_max)', (x_min, x_max), (y_min, y_max))
 
-    print('    ' + ' ' * (500 - x_min) + 'V')
+    print('    ' + ' ' * (501 - x_min) + 'V')
     for y in range(y_min, y_max + 1):
         output = '{:3} '.format(y)
-        for x in range(x_min, x_max + 1):
+        for x in range(x_min - 1, x_max + 2):
             if (x, y) in rocks:
                 output += '■'
             elif (x, y) in sand:
@@ -38,7 +42,6 @@ def visualize_grid(rocks: set, sand: set = set()) -> None:
         print(output)
 
 
-# @print_function(prefix = ' - ', run_time = True)
 def find_next_sand_position(rocks: set, sand: set, floor: bool = False) -> tuple:
     old_path: list = find_next_sand_position.last_path
     for _ in range(len(old_path)):
@@ -46,13 +49,10 @@ def find_next_sand_position(rocks: set, sand: set, floor: bool = False) -> tuple
         path = old_path[:]
         while True:
             path.append((x, y))
-            # print('(x, y)', (x, y))
-            # print('path =', path)
             if (x, y) in sand | rocks:
                 visualize_grid(rocks, sand)
                 raise(Exception('Can\'t place sand'))
             elif y >= find_next_sand_position.y_max:
-                # print('Max reached, {} >= {}'.format(y, find_next_sand_position.y_max))
                 if floor:
                     find_next_sand_position.last_path = path[:-1]
                     return (x, y)
@@ -72,51 +72,39 @@ def find_next_sand_position(rocks: set, sand: set, floor: bool = False) -> tuple
 find_next_sand_position.last_path = None
 find_next_sand_position.y_max = None
 
-# Start script
-lines = sys.stdin.read().strip().split('\n')
 
-# Find rock positions
-rocks = set()
-for line in lines:
-    line_pairs = [tuple(map(int, pair.split(','))) for pair in re.findall('[0-9]+,[0-9]+', line)]
-    for ((xs, ys), (xe, ye)) in zip(line_pairs[:-1], line_pairs[1:]):
-        dir = (min(1, max(-1, xe - xs)), min(1, max(-1, ye - ys)))
-        for idx in range(max(abs(xe - xs), abs(ys - ye)) + 1):
-            rocks.add((xs + dir[0] * idx, ys + dir[1] * idx))
-# visualize_grid(rocks)
+@print_function(prefix = ' - ', run_time = True)
+def solve(rocks: set, y_max: int, floor: bool) -> int:
+    find_next_sand_position.last_path = [(500, 0)]
+    find_next_sand_position.y_max = y_max
+    sand = set()
+    while True:
+        next_pos = find_next_sand_position(rocks, sand, floor)
+        if next_pos == None:
+            break
+        sand.add(next_pos)
+    visualize_grid(rocks, sand)
 
-# Calculate part 1
-find_next_sand_position.last_path = [(500, 0)]
-find_next_sand_position.y_max = max([pair[1] for pair in rocks])
-sand = set()
-while True:
-    next_pos = find_next_sand_position(rocks, sand, False)
-    if next_pos == None:
-        break
-    sand.add(next_pos)
-# visualize_grid(rocks, sand)
-print('Part 1:', len(sand))
+    return len(sand)
 
-# Calculate initial floor
-# x_min = min([pair[0] for pair in rocks | sand])
-# x_max = max([pair[0] for pair in rocks | sand])
-# y_max = max([pair[1] for pair in rocks | sand])
-# for x in range(x_min - 2, x_max + 3):
-#     rocks.add((x, y_max + 2))
-# visualize_grid(rocks)
 
-find_next_sand_position.last_path = [(500, 0)]
-find_next_sand_position.y_max = max([pair[1] for pair in rocks | sand]) + 1
-sand = set()
-while True:
-    next_pos = find_next_sand_position(rocks, sand, True)
-    sand.add(next_pos)
-    # rocks.add((next_pos[0] - 2, y_max + 2))
-    # rocks.add((next_pos[0] + 2, y_max + 2))
-    if next_pos == (500, 0):
-        break
-    # visualize_grid(rocks, sand)
-    if len(sand) % 500 == 0:
-        print(len(sand))
-visualize_grid(rocks, sand)
-print('Part 2:', len(sand))
+if __name__ == '__main__':
+    """Executed if file is executed but not if file is imported."""
+    
+    # Start script
+    lines = sys.stdin.read().strip().split('\n')
+
+    # Find rock positions
+    rocks = set()
+    for line in lines:
+        line_pairs = [tuple(map(int, pair.split(','))) for pair in re.findall('[0-9]+,[0-9]+', line)]
+        for ((xs, ys), (xe, ye)) in zip(line_pairs[:-1], line_pairs[1:]):
+            dir = (min(1, max(-1, xe - xs)), min(1, max(-1, ye - ys)))
+            for idx in range(max(abs(xe - xs), abs(ys - ye)) + 1):
+                rocks.add((xs + dir[0] * idx, ys + dir[1] * idx))
+    visualize_grid(rocks)
+    y_max = max([pair[1] for pair in rocks])
+
+    # Calculate result
+    print('Part 1:', solve(rocks, y_max, False))
+    print('Part 2:', solve(rocks, y_max + 1, True))
