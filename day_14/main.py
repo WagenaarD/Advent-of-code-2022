2540cc0d7fdd56183e9ee8b27e09afccffce5550
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-14 19:28:19 +0100

    day_14 cleanup

--- day_14/main.py
+++ day_14/main.py
@@ -20,12 +20,7 @@ parts 1 and 2 respectively.
 import sys
 sys.path.insert(0, '/'.join(__file__.replace('\\\\', '/').split('/')[:-2]))
 from _utils.print_function import print_function
-import itertools as it
-from dataclasses import dataclass, field
-from collections import defaultdict
 import re
-import numpy as np
-from pprint import pprint
 
 
 def visualize_grid(rocks: set, sand: set = set()) -> None:
