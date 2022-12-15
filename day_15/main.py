3f1259f379ff5e89ec31bca15d89d37bb1d09260
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-15 11:03:14 +0100

    day_15 part 2 working

--- day_15/main.py
+++ day_15/main.py
@@ -2,14 +2,14 @@
 Advent of code challenge 2022
 >> python3 main.py < in
 Start   - 
-Part 1  - 
-Part 2  - 
+Part 1  - 4737443 (ex 26)
+Part 2  - (ex 56000011)
 Cleanup - 
 """
 
 import sys
 sys.path.insert(0, '/'.join(__file__.replace('\\', '/').split('/')[:-2]))
-# from _utils.print_function import print_function
+from _utils.print_function import print_function
 # import itertools as it
 # from dataclasses import dataclass, field
 # from collections import defaultdict
@@ -18,26 +18,64 @@ import re
 from pprint import pprint
 
 
-lines = sys.stdin.read().strip().split('\n')
-y_target = 10 if len(lines) == 14 else 2000000
+def can_be_beacon(x: int, y: int, sensors: list) -> bool:
+    for (sx, sy, sd) in sensors:
+        coord_distance = abs(sx - x) + abs(sy - y)
+        if coord_distance <= sd:
+            return False
+    return True
 
-# lines = [list(map(int, re.findall('[0-9]+', line))) for line in sys.stdin.read().strip().split('\n')]
-blocked = set()
-beacons = set()
-for xs, ys, xb, yb in [list(map(int, re.findall('-?[0-9]+', line))) for line in lines]:
-    d_beacon = abs(xb - xs) + abs(yb - ys)
-    d_target = abs(y_target - ys)
-    # if d_target <= d_beacon:
-    for x in range(xs - (d_beacon - d_target), xs + (d_beacon - d_target) + 1):
-        blocked.add(x)
-    if yb == y_target:
-        beacons.add(xb)
 
-print('Part 1:', len(blocked - beacons), ' at row = ', y_target)
-    # else:
-    #     print(xs, ys, xb, yb, 'NO', d_target, d_beacon)
 
-    # width_at_target_y = (d_beacon - d_target) * 2 + 1
+@print_function(run_time = True)
+def solve_part_1(int_line):
+    y_target = 10 if len(int_lines) == 14 else 2000000
+
+    # 
+    blocked = set()
+    beacons = set()
+    for xs, ys, xb, yb in int_lines:
+        d_beacon = abs(xb - xs) + abs(yb - ys)
+        d_row = abs(y_target - ys)
+        for x in range(xs - (d_beacon - d_row), xs + (d_beacon - d_row) + 1):
+            blocked.add(x)
+        if yb == y_target:
+            beacons.add(xb)
+    return len(blocked - beacons)
+    # print('Part 1:', len(blocked - beacons), {'y': y_target})
+
+@print_function(run_time = True)
+def solve_part_2(int_lines):
+    # # The following is already too slow, we need to find a quicker way for part 2!:
+    # for x in range(xy_max + 1):
+    #     for y in range(xy_max + 1):
+    #         x + y
+
+    xy_max = 20 if len(int_lines) == 14 else 4000000
+
+    # New approach, scan only the edges of the sensor ranges
+    sensors = [(s[0], s[1], abs(s[0] - s[2]) + abs(s[1] - s[3])) for s in int_lines]
+    sensors.sort(key = lambda x: x[2])
+    for idx, (sx, sy, sd) in enumerate(sensors):
+        print('Progress = {:%}'.format(idx / len(sensors)))
+        for x in range(sx - (sd + 1), sx + (sd + 1) + 1):
+            if not 0 <= x <= xy_max:
+                continue
+            dy = (sd + 1) - abs(x - sx)
+            for y in {sy - dy, sy + dy}:
+                if not 0 <= y <= xy_max:
+                    continue
+                if can_be_beacon(x, y, sensors):
+                    return x * 4000000 + y
+
+if __name__ == '__main__':
+    """Executed if file is executed but not if file is imported."""
+    lines = sys.stdin.read().strip().split('\n')
+    int_lines = [list(map(int, re.findall('-?[0-9]+', line))) for line in lines]
+
+    print('Part 1:', solve_part_1(int_lines))
+    print('Part 1:', solve_part_2(int_lines))
+
 
 
 
