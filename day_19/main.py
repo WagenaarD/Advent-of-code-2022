90bc898592c80780eaae7e5a4acbbd949a8546a2
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-20 09:27:18 +0100

    day_19 more cleanup

--- day_19/main.py
+++ day_19/main.py
@@ -23,78 +23,62 @@ import math
 #  x DIDNT WORK: If increasing the cap doesnt improve total_score, quit
 
 
-@cache
-def dfs(blueprint, robot_cap, t = 24, robots = (1,0,0,0), resource = (0,0,0,0)):
+def run_dfs(blueprint, tmax = 24):
+    """
+    Second attempt at DFS solution.
+    Use a while loop instead of a recursive loop.
+    - Con: No easy caching
+    - Pro: Easier to keep track of overarching parameters
+
+    Optimization ideas:
+    - Don't assume the geode crusher always needs to be produced if possible, this is not always
+    optimal
+    - Calculate the max possible score and prune the tree if this is less than the already found
+    highest score
+    - The current highest score is the score of waiting out the rest of the time
+    - The max. possible score is the score for buying a geode crusher on every turn after this one
+    - The dfs prioritizes the paths buying expensive robots first, to help with this pruning
+    - This works because a lot of buying decisions are concentrated in the last few minutes as a lot
+    of resources are available then.
+    """
+    robot_cap = [max(bp[i] for bp in blueprint) for i in range(3)] + [tmax]
+    stack = [(tmax, (1,0,0,0), (0,0,0,0))]
     output = []
-    delta_ts = []
-    for rob in range(4):
-        # Optimziation: Don't build more robots than allowed
-        if robots[rob] >= robot_cap[rob]:
-            delta_ts.append(t)
-            continue
-        required = blueprint[rob]
-        dt = 0
-        for res in range(3):
-            # Test if we can already produce this
-            if required[res] == 0:
-                continue
-            # Test if we produce this at all
-            elif robots[res] == 0:
-                dt = t
-            # Calculate time to produce this
-            else:
-                dt = max(dt, 1, 1+math.ceil((required[res] - resource[res]) / robots[res]))
-        delta_ts.append(dt)
-    if min(delta_ts) == delta_ts[-1]:
-        rob == 3
-        if delta_ts[rob] < t:
-            # Investigate the result of buying this robot next
-            output += dfs(
-                blueprint,
-                robot_cap,
-                t - delta_ts[rob],
-                tuple(robots[i] + (1 if i == rob else 0) for i in range(4)),
-                tuple(resource[i] + robots[i] * delta_ts[rob] - blueprint[rob][i] \
-                    for i in range(4)),
-            )
-    else:
+    max_score = 0
+    while stack:
+        t, robots, resource = stack.pop()
         for rob in range(4):
-            if delta_ts[rob] < t:
-                # Investigate the result of buying this robot next
-                output += dfs(
-                    blueprint,
-                    robot_cap,
-                    t - delta_ts[rob],
+            # Don't build more robots than allowed
+            if robots[rob] >= robot_cap[rob]:
+                continue
+            required = blueprint[rob]
+            dt = 1
+            for res in range(3):
+                # Test if we can already produce this
+                if required[res] == 0: # should be <= resources[res]?
+                    continue
+                # Test if we produce this at all
+                elif robots[res] == 0:
+                    break
+                # Calculate time to produce this
+                else:
+                    dt = max(dt, 1, 1 + math.ceil((required[res] - resource[res]) / robots[res]))
+                    if dt >= t:
+                        break
+            else:
+                cur_score = resource[3] + robots[3] * t + ((t - dt) if rob == 3 else 0)
+                pot_score = cur_score + sum([i+1 for i in range((t - dt))])
+                if pot_score < max_score:
+                    continue
+                max_score = max(cur_score, max_score)
+                stack.append((
+                    t - dt, 
                     tuple(robots[i] + (1 if i == rob else 0) for i in range(4)),
-                    tuple(resource[i] + robots[i] * delta_ts[rob] - blueprint[rob][i] \
-                        for i in range(4)),
-                )
-    # If nothing is produced, return the result of waiting out the rest of the time
-    resource = tuple(resource[i] + robots[i] * t for i in range(4))
-    output.append(
-        (resource, robots, t)
-    )
-    return output
-
-
-@print_function(run_time = True, include_args = False)
-def run_dfs(blueprint, t = 24):
-    dfs.cache_clear()
-    max_cost = [max(bp[i] for bp in blueprint) for i in range(4)]
-    max_cost[-1] = t
-    cap = tuple(max_cost)
-    while True:
-        output = dfs(tuple(blueprint), cap, t)
-        top_score = max([out[0][3] for out in output])
-        max_robots_used = [max([out[1][rob] for out in output if out[0][3] == top_score]) \
-            for rob in range(4)]
-        new_cap = tuple(min(max_cost[i], max(cap[i], max_robots_used[i] + 1)) for i in range(4))
-        if new_cap == cap:
-            break
-        else:
-            cap = new_cap
-
-    return top_score
+                    tuple(resource[i] + robots[i] * dt - blueprint[rob][i] for i in range(4))
+                ))
+        output.append(resource[3] + robots[3] * t)
+    print('no_paths:', len(output))
+    return max(output)
 
 
 @print_function(run_time = True, include_args = False)
