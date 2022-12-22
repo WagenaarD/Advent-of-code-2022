c3f502fa6fb3b81181d219f7b8d084c7a92b49de
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-22 13:58:39 +0100

    day_22 part 2

--- day_22/main.py
+++ day_22/main.py
@@ -3,7 +3,7 @@ Advent of code challenge 2022
 >> python3 main.py < in
 Start   - 
 Part 1  - 67390
-Part 2  - 
+Part 2  - 95291
 Cleanup - 
 """
 
@@ -41,7 +41,14 @@ def rotate(dir, lr):
     else:
         return (dir[1], -dir[0])
 
-
+def out_of_bounds(x, y, lines):
+    if 0 <= x < len(lines[0]) and 0 <= y < len(lines):
+        return lines[y][x] == ' '
+    return True
+    # if not (0 < y <= len(lines) and 0 < x <= len(lines[0])):
+    #     return True
+    # else:
+    #     return lines[y][x] == ' '
 
 lines = sys.stdin.read().split('\n')
 
@@ -52,33 +59,112 @@ width = max([len(line) for line in lines])
 height = len(lines)
 lines = [line + ' ' * (width - len(line)) for line in lines]
 
-pprint(list(enumerate(lines)))
-print('                1    1    2')
-print('      0    5    0    5    0')
-print('\n'.join(['{:3} - {}'.format(idx, line) for idx, line in enumerate(lines)]))
+# pprint(list(enumerate(lines)))
+# print('                1    1    2')
+# print('      0    5    0    5    0')
+# print('\n'.join(['{:3} - {}'.format(idx, line) for idx, line in enumerate(lines)]))
 
 dir = (1, 0)
 x, y = [lines[0].index('.'), 0]
 
-for command in input:
-    print('(pos, dir, command) =', ((x, y), dir, command)) 
+for command_idx, command in enumerate(input[:]):
+    print('{}: (pos, dir, command) = {}'.format(command_idx, ((x, y), dir, command)))
     if command in ['R', 'L']:
         dir = rotate(dir, command)
         continue
     for idx in range(int(command)):
+        # Move one step at a time
         dx, dy = dir
-        # Manage wrapping around
-        while lines[(y + dy) % height][(x + dx) % width] == ' ':
-            dx += dir[0]
-            dy += dir[1]
-        dx = (x + dx) % width - x
-        dy = (y + dy) % height - y
+        next_dir = dir
+        next_x, next_y = x + dx, y + dy
+        # # Manage wrapping around (Part 1)
+        # while lines[(y + dy) % height][(x + dx) % width] == ' ':
+        #     dx += dir[0]
+        #     dy += dir[1]
+        # dx = (x + dx) % width - x
+        # dy = (y + dy) % height - y
+
+        if out_of_bounds(next_x, next_y, lines):
+            # First find the current face
+            face_x = x % 50
+            face_y = y % 50
+            if x // 50 == 0 and y // 50 == 2:
+                # Black
+                if dx < 0:
+                    next_x, next_y = 50, 49 - face_y
+                    next_dir = (1, 0)
+                elif dy < 0:
+                    next_x, next_y = 50, 50 + face_x
+                    next_dir = (1, 0)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            elif x // 50 == 0 and y // 50 == 3:
+                # Green
+                if dx < 0:
+                    next_x, next_y = 50 + face_y, 0
+                    next_dir = (0, 1)
+                elif dx > 0:
+                    next_x, next_y = 50 + face_y, 149
+                    next_dir = (0, -1)
+                elif dy > 0:
+                    next_x, next_y = 100 + face_x, 0
+                    next_dir = (0, 1)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            elif x // 50 == 1 and y // 50 == 0:
+                # White
+                if dx < 0:
+                    next_x, next_y = 0, 149 - face_y
+                    next_dir = (1, 0)
+                elif dy < 0:
+                    next_x, next_y = 0, 150 + face_x
+                    next_dir = (1, 0)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            elif x // 50 == 1 and y // 50 == 1:
+                # Blue
+                if dx < 0:
+                    next_x, next_y = face_y, 100
+                    next_dir = (0, 1)
+                elif dx > 0:
+                    next_x, next_y = 100 + face_y, 49
+                    next_dir = (0, -1)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            elif x // 50 == 1 and y // 50 == 2:
+                # Yellow
+                if dx > 0:
+                    next_x, next_y = 149, 49 - face_y
+                    next_dir = (-1, 0)
+                elif dy > 0:
+                    next_x, next_y = 49, 150 + face_x
+                    next_dir = (-1, 0)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            elif x // 50 == 2 and y // 50 == 0:
+                # Red
+                if dx > 0:
+                    next_x, next_y = 99, 149 - face_y
+                    next_dir = (-1, 0)
+                elif dy < 0:
+                    next_x, next_y = face_x, 199
+                    next_dir = (0, -1)
+                elif dy > 0:
+                    next_x, next_y = 99, 50 + face_x
+                    next_dir = (-1, 0)
+                else:
+                    raise(Exception('WTF: (x, y) (next_x, next_y) {} {}'.format((x, y), (next_x, next_y))))
+            dx, dy = next_x - x, next_y - y
+            print('  Changed cube face: {} -> {}'.format((x, y, dir), (next_x, next_y, next_dir)))
+            if (out_of_bounds(next_x, next_y, lines)):
+                raise(Exception(('  Out of bounds'))
 
         # Manage walls
-        if lines[y + dy][x + dx] == '#':
+        if lines[next_y][next_x] == '#':
+            print('  Brick at {}'.format((next_x, next_y)))
             break
-        elif lines[y + dy][x + dx] == '.':
-            x, y = (x + dx, y + dy)
+        elif lines[next_y][next_x] == '.':
+            x, y, dir = next_x, next_y, next_dir
 print('(pos, dir, command) =', ((x, y), dir, 'DONE'))
 
 print('Final score:', 1000 * (y + 1) + 4 * (x + 1) + DIR_SCORE[dir])
