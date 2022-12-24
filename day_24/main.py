9090ff697bc3740ea175e882e50bbe93b6b0ddc2
Dirk Wagenaar <ir.d.wagenaar@gmail.com>
2022-12-24 11:31:15 +0100

    day_24 cleanup

--- day_24/main.py
+++ day_24/main.py
@@ -17,25 +17,7 @@ import re
 import numpy as np
 from pprint import pprint
 from functools import cache
-
-
-def print_grid(blizzards, start, end, width, height, stack = []):
-    print('■' * (start[1] + 1) + '.' + '■' * (width - start[1]))
-    for row in range(height):
-        output = '■'
-        for col in range(width):
-            bs = [b for b in blizzards if b[0] == row and b[1] == col]
-            if len(bs) > 1:
-                output += str(len(bs))
-            elif len(bs) == 1:
-                output += bs[0][2]
-            elif (row, col) in stack:
-                output += 'E'
-            else:
-                output += '.'
-        print(output + '■')
-    print('■' * (end[1] + 1) + '.' + '■' * (width - end[1]))
-    print('')
+from datetime import datetime
 
 
 DIRS = {
@@ -47,59 +29,80 @@ DIRS = {
 MOVES = ((0, 0), (1, 0), (-1, 0), (0, 1), (0, -1))
 
 
+def print_grid(blizzards, start, end, width, height, stack = []):
+    """
+    Visualize the grid similarly to the example output. Add an 'E' to every coordinate present in 
+    stack
+    """
+    for row in range(-1, height + 1):
+        output = '■'
+        for col in range(width):
+            blizs = [bliz for bliz in blizzards if (row, col) == (bliz[0], bliz[1])]
+            if len(blizs) > 1:
+                output += str(len(blizs))
+            elif len(blizs) == 1:
+                output += blizs[0][2]
+            elif (row, col) in stack:
+                output += 'E'
+            elif (row, col) in (start, end):
+                output += '.'
+            elif row in (-1, height):
+                output += '■'
+            else:
+                output += '.'
+        print(output + '■')
+    print('')
+
+
 def move_blizzards(blizzards, width, height):
-    new_bs = []
-    for r, c, dir in blizzards:
-        dr, dc = DIRS[dir]
-        rr, cc = (r + dr) % height, (c + dc) % width
-        new_bs.append((rr, cc, dir))
-    return new_bs
+    new_blizzards = []
+    for row, col, dir in blizzards:
+        drow, dcol = DIRS[dir]
+        nrow, ncol = (row + drow) % height, (col + dcol) % width
+        new_blizzards.append((nrow, ncol, dir))
+    return new_blizzards
 
 
 @print_function()
 def bfs(blizzards, start, end, width, height):
     t = 0
     stack = {start}
-    # print_grid(blizzards, start, end, width, height, stack)
+    if width < 10: print_grid(blizzards, start, end, width, height, stack)
     while len(stack) >= 1:
-        # print('== Minute {}, stack {} =='.format(t, stack))
-        # print_grid(blizzards, start, end, width, height, stack)
+        if width < 10: print('== Minute {}, stack {} =='.format(t, stack))
+        if width < 10: print_grid(blizzards, start, end, width, height, stack)
         t += 1
         blizzards = move_blizzards(blizzards, width, height)
-        # for row, col in stack[:]:
+        blizzard_locations = {(bliz[0], bliz[1]) for bliz in blizzards}
         old_stack = stack
         stack = set()
         for row, col in old_stack:
-            # row, col = stack.pop(0)
             for drow, dcol in MOVES:
                 nrow, ncol = row + drow, col + dcol
                 if (nrow, ncol) == end:
                     return (t, blizzards)
                 if 0 <= nrow < height and 0 <= ncol < width or (nrow, ncol) == start:
-                    if len([b for b in blizzards if b[0] == nrow and b[1] == ncol]) == 0:
+                    if (nrow, ncol) not in blizzard_locations:
                         stack.add((nrow, ncol))
                         
 
-    
+if __name__ == '__main__':
+    """Executed if file is executed but not if file is imported."""
+    lines = sys.stdin.read().strip().split('\n')
+    # lines[row][col]
+    # Blizzards in grid starting at [0][0]
+    start = (-1, lines[0].index('.') - 1)
+    end = (len(lines) - 2, lines[-1].index('.') - 1)
+    width = len(lines[0])-2
+    height = len(lines) - 2
+    blizzards = []
+    for row, line in enumerate(lines[1:-1]):
+        for col, char in enumerate(line[1:-1]):
+            if char != '.':
+                blizzards.append((row, col, char))
 
-lines = sys.stdin.read().strip().split('\n')
-# lines[row][col]
-# Blizzards in grid starting at [0][0]
-start = (-1, lines[0].index('.') - 1)
-end = (len(lines) - 2, lines[-1].index('.') - 1)
-width = len(lines[0])-2
-height = len(lines) - 2
-blizzards = []
-for row, line in enumerate(lines[1:-1]):
-    for col, char in enumerate(line[1:-1]):
-        if char != '.':
-            blizzards.append((row, col, char))
-
-t_0, blizzards = bfs(blizzards, start, end, width, height)
-
-t_1, blizzards = bfs(blizzards, end, start, width, height)
-t_2, blizzards = bfs(blizzards, start, end, width, height)
-print(t_0, t_1, t_2, t_0 + t_1 + t_2)
-
-
-
+    t_0, blizzards = bfs(blizzards, start, end, width, height)
+    t_1, blizzards = bfs(blizzards, end, start, width, height)
+    t_2, blizzards = bfs(blizzards, start, end, width, height)
+    print('Part 1:', t_0)
+    print('Part 2:', t_0 + t_1 + t_2)
